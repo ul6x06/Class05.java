98e026e12084935820a95e956ff5b0cd4623a818
ul6x06 <91857915+ul6x06@users.noreply.github.com>
2021-10-10 23:43:39 +0800

    Add files via upload

---
+++ Class05.java
@@ -0,0 +1,14 @@
+package Java08;
+
+public class Class05 {
+
+	public static void main(String[] args) {
+		CBox box = new CBox();
+		box.length = 1;
+		box.width = 1;
+		box.height = 1;
+		box.showAll();
+
+	}
+
+}
